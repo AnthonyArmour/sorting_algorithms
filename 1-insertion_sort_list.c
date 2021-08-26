4134b9f4d626b3ee4e41f230f57bf00f5e53ed21
AnthonyArmour <anthonydarmour@gmail.com>
2021-08-26 21:02:47 +0000

    push insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts  a doubly linked list of integers
+ * @list: the list to sort
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL, *next = NULL;
+
+	if (!list)
+		return;
+	current = *list;
+	while (current && current->next)
+	{
+		next = current->next;
+		if (current->n > next->n)
+		{
+			if (next->next)
+				next->next->prev = current;
+			else
+				*list = next;
+			current->prev = next;
+			current->next = next->next;
+			next->next = current;
+			print_list(*list);
+			current = *list;
+		}
+		else
+			current = current->next;
+	}
+}
